2fccaff61c5af6587926c77f0b51e8de55369645
Nico Felbinger <26925347+felbinger@users.noreply.github.com>
2021-08-25 22:03:42 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cat <<EOF > index.html
+<html>
+<head><title>Files:</title></head>
+<body>
+<h1>Index</h1>
+EOF
+
+# TODO walk through directory
+for file in $(ls | grep -v build.sh); do
+    echo -e "<a href='${file}'>${file}</a></br>" >> index.html
+done
+
+cat <<EOF >> index.html
+</pre><hr></body>
+</html>
+EOF
